420d9db82239c133e90aeb629d17a90952bcbc93
“Jussie247” <justus.schultzke@gmail.com>
2025-12-12 03:08:52 +0100

    fixed maybe?

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // WICHTIG: Repo-Name hier eintragen
-  base: '/justus-portfolio/',
+
+  base: '/Portfolio/',
+  build: {
+    outDir: 'docs',
+  },
 });
